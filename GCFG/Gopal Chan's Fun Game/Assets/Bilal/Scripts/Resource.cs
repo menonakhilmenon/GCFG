d17f236a1d0d147ecaa20800b6ad926a55d76410
Mohd-Bilal <bilaljaleel123@gmail.com>
2020-02-01 04:25:02 -0800

    add ontrigger property

---
+++ GCFG/Gopal Chan's Fun Game/Assets/Bilal/Scripts/Resource.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace bilal
+{
+
+    public class Resource : MonoBehaviour
+    {
+        public Action onCollect;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            gameObject.SetActive(false);
+            onCollect?.Invoke();
+        }
+    }
+
+}
