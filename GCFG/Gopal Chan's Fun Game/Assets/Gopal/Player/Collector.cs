2855b905660845b909192d499fab65aaeacf3daf
Gopal9816 <gopalkurup@gmail.com>
2020-02-01 23:45:52 +0530

    Implemented Pickup

---
+++ GCFG/Gopal Chan's Fun Game/Assets/Gopal/Player/Collector.cs
@@ -0,0 +1,16 @@
+﻿using bilalAdarsh;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Gopal
+{
+    public class Collector : MonoBehaviour
+    {
+        public Action<PickUp> CollectItem;
+
+    }
+}
+
