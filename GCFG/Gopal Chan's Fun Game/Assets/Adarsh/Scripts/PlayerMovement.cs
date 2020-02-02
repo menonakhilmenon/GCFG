25253790b0ba8708cf5a24314f40d823212bb15e
adarshnp <adarshnp49@gmail.com>
2020-02-02 16:08:07 +0530

    movement

--- GCFG/Gopal Chan's Fun Game/Assets/Adarsh/Scripts/PlayerMovement.cs
+++ GCFG/Gopal Chan's Fun Game/Assets/Adarsh/Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 6.0f;
     public float tilt = 5.0f;
+    public float mouseSensitivity = 5f;
     private Vector3 moveDirection = Vector3.zero;
     void Start()
     {
@@ -14,8 +15,10 @@ public class PlayerMovement : MonoBehaviour
     {
             moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, -Input.GetAxis("Vertical"));
             moveDirection *= speed;
-			// moveDirection.y -= gravity * Time.deltaTime;
-			transform.Translate(moveDirection * Time.deltaTime);
-            transform.Rotate(new Vector3(Input.GetAxis("Horizontal"), 0.0f,0.0f) * tilt * Time.deltaTime);
+        // moveDirection.y -= gravity * Time.deltaTime;
+            var rot = Input.GetAxis("Mouse X");
+            transform.eulerAngles += (Vector3.up * rot * mouseSensitivity * Time.deltaTime);
+			transform.Translate(moveDirection  * Time.deltaTime);
+            
 	}
 }
