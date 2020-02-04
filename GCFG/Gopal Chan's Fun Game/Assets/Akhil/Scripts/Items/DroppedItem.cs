5a73d60c05fd5dc8acc82adca4bf83164da2bf1a
Akhil Menon <menonakhilmenon@gmail.com>
2020-02-04 18:15:10 +0530

    Networked PickUP

--- GCFG/Gopal Chan's Fun Game/Assets/Akhil/Scripts/Items/DroppedItem.cs
+++ GCFG/Gopal Chan's Fun Game/Assets/Akhil/Scripts/Items/DroppedItem.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 namespace GCFG
 {
     [RequireComponent(typeof(PhotonView))]
-    public class DroppedItem : MonoBehaviourPun
+    public class DroppedItem : MonoBehaviourPun,IPunObservable
     {
         [SerializeField]
         private Item itemType = null;
@@ -15,11 +15,30 @@ namespace GCFG
         private int _count = 1;
         public int Count { get => _count; set => _count = value; }
 
+        private int lastSentCount = 1;
+
         public void NetworkPickUp() 
         {
             if (PlayerManager.instance.LocalPlayerInventory.TryAdd(itemType, Count)) 
             {
                 photonView.RPC(nameof(ItemPickUpRPC), RpcTarget.AllBufferedViaServer, PhotonNetwork.LocalPlayer.ActorNumber);
+            }
+        }
+
+        public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+        {
+            if (stream.IsReading) 
+            {
+                var newCount = (int)stream.ReceiveNext();
+                Count = newCount;
+            }
+            else 
+            {
+                if(lastSentCount!= Count) 
+                {
+                    lastSentCount = Count;
+                    stream.SendNext(Count);
+                }
             }
         }
 
---
+++ GCFG/Gopal Chan's Fun Game/Assets/Akhil/Scripts/Networking/PickupSpawner.cs
@@ -0,0 +1,38 @@
+﻿using bilalAdarsh;
+using GCFG;
+using NaughtyAttributes;
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PhotonView))]
+public class PickupSpawner : MonoBehaviourPun
+{
+    [SerializeField]
+    private PhotonObjects photonObjects = null;
+
+    [Button("Spawn Shit")]
+    public void SpawnAtOrigin() 
+    {
+        SpawnPickup(photonObjects.GetObject(0) as Item, 4, transform.position + Vector3.up * 1f, transform.rotation);
+    }
+
+    public void SpawnPickup(Item  itemType, int count,Vector3 position,Quaternion rotation) 
+    {
+        photonView.RPC(nameof(SpawnPickupRPC), RpcTarget.All, photonObjects.GetIndex(itemType), count, position, rotation);
+    }
+
+    [PunRPC]
+    private void SpawnPickupRPC(int itemIndex,int count,Vector3 position,Quaternion rotation) 
+    {
+        if (!PhotonNetwork.IsMasterClient)
+            return;
+        var item = photonObjects.GetObject(itemIndex) as Item;
+        if (item != null) 
+        {
+            var obj = PhotonNetwork.Instantiate(item.dropModel.name, position, rotation);
+            obj.GetComponent<DroppedItem>().Count = count;
+        }
+    }
+}
