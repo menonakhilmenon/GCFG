e895174506626a6c1912741483b7e2d8a19c53d0
Akhil Menon <menonakhilmenon@gmail.com>
2020-02-06 23:07:40 +0530

    Fixed xplayer bug

---
+++ GCFG/Gopal Chan's Fun Game/Assets/Akhil/Scripts/CraftRecipe.cs
@@ -0,0 +1,28 @@
+﻿using bilalAdarsh;
+using NaughtyAttributes;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GCFG
+{
+    [System.Serializable]
+    public class CraftRequirement
+    {
+        [SerializeReference]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2235:Mark all non-serializable fields", Justification = "Serialized as Reference")]
+        public Item item = null;
+        public int amount = 1;
+    }
+
+
+
+    [CreateAssetMenu]
+    public class CraftRecipe : ScriptableObject
+    {
+        public Item result = null;
+        [ReorderableList]
+        public List<CraftRequirement> craftRequirements = null;
+
+    }
+}
---
+++ GCFG/Gopal Chan's Fun Game/Assets/CraftEvent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GCFG
+{
+    [CreateAssetMenu]
+    public class CraftEvent : ScriptableGameEvent
+    {
+        public void InvokeWithCraftRecipe(CraftRecipe recipe) 
+        {
+            Invoke(recipe);
+        }
+    }
+}
